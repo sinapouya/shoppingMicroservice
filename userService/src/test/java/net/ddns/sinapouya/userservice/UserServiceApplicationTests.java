edcd085dca153af85293897626ce9b0fe1ef05e2
sina pouya <pooya.sina@gmail.com>
2023-12-28 20:02:21 +0330

    add test class for user microservice to get token

--- userService/src/test/java/net/ddns/sinapouya/userservice/UserServiceApplicationTests.java
+++ userService/src/test/java/net/ddns/sinapouya/userservice/UserServiceApplicationTests.java
@@ -1,13 +1,42 @@
 package net.ddns.sinapouya.userservice;
 
+import dasniko.testcontainers.keycloak.KeycloakContainer;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@Testcontainers
+@AutoConfigureMockMvc
 class UserServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
+	static final KeycloakContainer keycloak;
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	static {
+		keycloak = new KeycloakContainer("quay.io/keycloak/keycloak:22.0.3")
+				.withRealmImportFile("./keycloak.json");
+		keycloak.start();
 	}
+	@Test
+	void generateTokenTest() throws Exception {
+		mockMvc.perform(post("/api/user")
+				.contentType("application/json")
+				.content("{\"clientId\":\"spring-client\"," +
+						" \"userName\":\"user1\"," +
+						" \"password\":\"pass123\"," +
+						" \"grantType\":\"password\"}"))
+				.andExpect(status().isOk());
+	}
+
+
 
 }
